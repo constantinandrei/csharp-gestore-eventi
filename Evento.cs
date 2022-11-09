9d3b4ef29886d82b833ee4d16785221a1420ef7a
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-11-09 14:55:14 +0100

    scritto costruttore per evento

--- Evento.cs
+++ Evento.cs
@@ -2,21 +2,63 @@
 
 public class Evento
 {
-    public string Titolo { get; set; }
-    private DateTime _data;
-    public DateTime Data { get
+    private string _titolo;
+    public string Titolo
+    {
+
+        get
         {
-            return _data; 
-        } 
-        set 
+            return _titolo;
+        }
+        set
+        {
+            if (value == null || value.Equals(""))
+            {
+                throw new GestoreEventiException("Il nome del evento non può essere vuoto");
+            }
+            _titolo = value;
+        }
+    }
+    private DateTime _data;
+    public DateTime Data
+    {
+        get
+        {
+            return _data;
+        }
+        set
         {
             if (DateTime.Now > value)
                 throw new GestoreEventiException("Non è possibile inserire un evento in una data passata");
             _data = value;
         }
     }
-    public int MaxPosti { get; }
+    private int _maxPosti;
+
+    
+
+    public int MaxPosti
+    {
+        get
+        {
+            return _maxPosti;
+        }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new GestoreEventiException("Il numero di posti disponibili deve essere almeno 1");
+            }
+        }
+    }
     public int PostiPrenotati { get; }
+    public Evento(string titolo, DateTime data, int maxPosti)
+    {
+        Titolo = titolo;
+        Data = data;
+        MaxPosti = maxPosti;
+        PostiPrenotati = 0;
+    }
 }
 
 
--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿
 using csharp_gestore_eventi.Exceptions;
 
-Evento evento = new Evento();
-evento.Data = new DateTime(2022, 11, 21);
+Evento evento = new Evento("Evento", new DateTime(2022, 11, 21, 21, 00, 00), 2);
+
+Console.WriteLine(evento.Data);
 
 
 
