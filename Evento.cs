6c060fa8805470bdb35240817a6136de9ba871bb
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-11-09 14:32:51 +0100

    realizzato setter per data evento

---
+++ Evento.cs
@@ -0,0 +1,24 @@
+﻿using csharp_gestore_eventi.Exceptions;
+
+public class Evento
+{
+    public string Titolo { get; set; }
+    private DateTime _data;
+    public DateTime Data { get
+        {
+            return _data; 
+        } 
+        set 
+        {
+            if (DateTime.Now > value)
+                throw new GestoreEventiException("Non è possibile inserire un evento in una data passata");
+            _data = value;
+        }
+    }
+    public int MaxPosti { get; }
+    public int PostiPrenotati { get; }
+}
+
+
+
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using csharp_gestore_eventi.Exceptions;
+
+Evento evento = new Evento();
+evento.Data = new DateTime(2022, 11, 21);
+
+
+
