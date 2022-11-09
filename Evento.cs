f6af85680c1268b6bdfd408548d7f0e9f3a6f670
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-11-09 15:20:55 +0100

    completato milestone 1

--- Evento.cs
+++ Evento.cs
@@ -73,6 +73,23 @@ public class Evento
             throw new GestoreEventiException("Non ci sono abbastanza posti diponibili per questo evento");
         PostiPrenotati += posti;
     }
+
+    public void DisdiciPosti(int posti)
+    {
+        if (posti < 1)
+            throw new GestoreEventiException("Il numero di posti deve essere almeno 1");
+        if (DateTime.Now > Data)
+               throw new GestoreEventiException("Questo evento è già terminato, non è più possibile disdire le prenotazioni");
+        if (PostiPrenotati < posti)
+            throw new GestoreEventiException("Non è possibilie disdire più posti di quelli prenotati");
+        
+        PostiPrenotati -= posti;
+    }
+
+    public override string ToString()
+    {
+        return Data.ToString("dd/MM/yyyy") + " - " + Titolo;
+    }
 }
 
 
--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿
 using csharp_gestore_eventi.Exceptions;
 
-Evento evento = new Evento("Evento", new DateTime(2022, 11, 21, 21, 00, 00), 2);
-evento.PrenotaPosti(2);
+Evento evento = new Evento("Evento", new DateTime(2022, 11, 21, 21, 00, 00), 9);
+evento.PrenotaPosti(7);
+evento.DisdiciPosti(3);
 Console.WriteLine(evento.PostiPrenotati);
+Console.WriteLine(evento.ToString());
 
 
 
